4a5cfec3bc289697c70568425de89e5eafa9d704
shubham <55156409+agrawalshubham01@users.noreply.github.com>
2020-02-26 21:02:31 +0530

    Update adadelta.py

--- adadelta.py
+++ adadelta.py
@@ -1,40 +1,38 @@
 import numpy as np
 #to minimise f=x**2+y**2+z**2
 a=np.random.rand(3,1)
+a=np.float64(a)
 print("inital value of x,y,z are ")
 print(a)
 #a is all variables
-#n, m, q are for randomizing the functions
 sumdelx2=0
+sumdelx2=np.float64(sumdelx2)
 #sumdelx2 is for rms of delta x
 #df is partial differentiation array
 # df2 is partial differentiation square
 k=np.zeros((3,1))
 # k is running sum of df2
-l=np.zeros((3,1))
-# l is running sum of
-n=np.random.normal()
-m=np.random.normal()
-q=np.random.normal()
-df=np.array([[2*n],[2*m],[2*q]])
-df2=np.array([[4*n**2],[4*m**2],[4*q**2]])
+g=np.random.rand(3,1)
+df=2*g
+df2=df**2
 k=0.9*k+0.1*df2
 c=(0.9)
 b=(k+0.00001)**1/2
+b=np.float64(b)
 da=np.dot((c/b).T,df)
-sumdelx2=0.9*sumdelx2 + 0.1*da**2
+da=np.float64(da)
+sumdelx2=0.9*sumdelx2 + 0.1*np.square(da)
 a=a-da
-for i in range (0,999):
-    n=np.random.normal()
-    m=np.random.normal()
-    q=np.random.normal()
-    df=np.array([[2*n],[2*m],[2*q]])
-    df2=np.array([[4*n**2],[4*m**2],[4*q**2]])
+for i in range (0,1):
+    g=np.random.rand(3,1)
+    df=2*g
+    df2=df**2
     k=0.9*k+0.1*df2
     c=(sumdelx2+0.00001)**1/2
     b=(k+0.00001)**1/2
     da=np.dot((c/b).T,df)
     a=a-da
-    sumdelx2=0.9*sumdelx2 + 0.1*da**2
+    sumdelx2=0.9*sumdelx2 + 0.1*np.square(da)
+a = a.astype('float64')
 print("optimised value of x,y,z are ")
 print(a)
