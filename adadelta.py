b6ddaf63acff0c96157bae6fbe51cdd91515da33
shubham <55156409+agrawalshubham01@users.noreply.github.com>
2020-02-26 21:12:52 +0530

    Update adadelta.py

--- adadelta.py
+++ adadelta.py
@@ -1,6 +1,6 @@
 import numpy as np
 #to minimise f=x**2+y**2+z**2
-a=np.random.rand(3,1)
+a=np.random.randn(3,1)
 a=np.float64(a)
 print("inital value of x,y,z are ")
 print(a)
@@ -12,7 +12,7 @@ sumdelx2=np.float64(sumdelx2)
 # df2 is partial differentiation square
 k=np.zeros((3,1))
 # k is running sum of df2
-g=np.random.rand(3,1)
+g=np.random.randn(3,1)
 df=2*g
 df2=df**2
 k=0.9*k+0.1*df2
@@ -24,7 +24,7 @@ da=np.float64(da)
 sumdelx2=0.9*sumdelx2 + 0.1*np.square(da)
 a=a-da
 for i in range (0,1):
-    g=np.random.rand(3,1)
+    g=np.random.randn(3,1)
     df=2*g
     df2=df**2
     k=0.9*k+0.1*df2
